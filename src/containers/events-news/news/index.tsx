4a495e6a3fb753f42de0929b6d471f2ba63c6c2e
Pral2a <guillem@iaac.net>
2025-07-04 17:58:48 +0200

    Update index.tsx

--- src/containers/events-news/news/index.tsx
+++ src/containers/events-news/news/index.tsx
@@ -1,13 +1,17 @@
 import NewsList from '@/containers/events-news/news/list';
-
 import Wrapper from '@/components/ui/wrapper';
+import type { News as NewsType } from '@/data/news';
 
-export default function News() {
+type NewsProps = {
+  news: NewsType[];
+};
+
+export default function News({ news }: NewsProps) {
   return (
     <div className="bg-grey-800">
       <Wrapper className="space-y-8 lg:space-y-16">
         <h4 className="text-xl text-white lg:text-3xl">News</h4>
-        <NewsList />
+        <NewsList news={news} />
       </Wrapper>
     </div>
   );
